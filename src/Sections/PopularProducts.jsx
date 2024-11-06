43c2f6cfc80c5086a3ba8a5ad0bab1dc37cad35b
vinothini <vinothiniciet@gmail.com>
2024-11-06 16:15:12 +0400

    Added Popular productsCard

--- src/Sections/PopularProducts.jsx
+++ src/Sections/PopularProducts.jsx
@@ -1,8 +1,39 @@
-
-
+import { products} from "../constant";
+import PopularProductCard from "../Components/PopularProductCard";
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
+     <section id="products"
+     className="max-container max-sm:mt-12"
+     >
+        <div className="flex flex-col justify-start gap-5">
+          <h2 className="text-4xl font-palanquin font-bold">
+            
+            Our <span className="text-coral-red">Popular </span> Products
+
+          </h2>
+
+          <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray"> Experience top-notch quality with our styles and selections.Discover a world of comport , design and value </p>
+
+        </div>
+
+        <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+          {
+            products.map((eachProduct,index)=>(
+
+              <PopularProductCard 
+              key={index} 
+              imgURL={eachProduct.imgURL}
+               name={eachProduct.name}
+               price={eachProduct.price}
+
+              />
+
+            ))
+          }
+
+        </div>
+
+     </section>
   )
 }
 
